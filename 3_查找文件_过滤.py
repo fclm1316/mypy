015d7056a6b5b2612f371e03b34589b4f8969042
fclm1316 <fbfclm@qq.com>
2018-03-09 14:53:42 +0800

    默认的

--- 3_查找文件_过滤.py
+++ 3_查找文件_过滤.py
@@ -2,6 +2,7 @@
 import sys
 import os
 import glob
+import re
 input_path = sys.argv[1]
 all_file_path = glob.glob(os.path.join(input_path,'*.txt'))
 print('所有文件1：')
@@ -10,14 +11,18 @@ print(all_files)
 print('所有文件2：')
 # os.walk(top[, topdown=True[, onerror=None[, followlinks=False]]])
 # top -- 根目录下的每一个文件夹(包含它自己), 产生3-元组 (dirpath, dirnames, filenames)【文件夹路径, 文件夹名字, 文件名】。
-# topdown --可选，为True或者没有指定, 一个目录的的3-元组将比它的任何子文件夹的3-元组先产生 (目录自上而下)。如果topdown为 False, 一个目录的3-元组将比它的任何子文件夹的3-元组后产生 (目录自下而上)。
+# topdown --可选，为True或者没有指定, 一个目录的的3-元组将比它的任何子文件夹的3-元组先产生 (目录自上而下)。
+# 如果topdown为 False, 一个目录的3-元组将比它的任何子文件夹的3-元组后产生 (目录自下而上)。
 # onerror -- 可选，是一个函数; 它调用时有一个参数, 一个OSError实例。报告这错误后，继续walk,或者抛出exception终止walk。
 # followlinks -- 设置为 true，则通过软链接访问目录。
-for root,dirs,files in os.walk(input_path):
-    for walk_dirs in dirs:
-        print(os.path.join(root,walk_dirs))
-    for walk_files in files:
-        print(os.path.join(root,walk_files))
+for w_path, w_dirs, w_files in os.walk(input_path):
+    for walk_dirs in w_dirs:
+        print(os.path.join(w_path, walk_dirs))
+    for walk_files in w_files:
+        files_all = os.path.join(w_path, walk_files)
+#str.endswith() str.startswith() 以什么开头，以什么结尾
+        if files_all.endswith('.sh') == True:
+            print(files_all)
 
 print('查找的txt文件有：')
 print(all_file_path)
@@ -32,3 +37,5 @@ for names in all_file_path2:
         print(all_names)
         print(ss)
 #没办法过滤
+
+
