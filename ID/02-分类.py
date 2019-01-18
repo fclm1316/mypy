90f3ab2ba9801271365ce2d36110b57cf4baaff7
fclm1316 <fbfclm@qq.com>
2019-01-18 13:47:31 +0800

    new

--- ID/02-分类.py
+++ ID/02-分类.py
@@ -6,7 +6,7 @@ from multiprocessing import Pool
 import os
 import glob
 input_filename = 'D:/2000Wnew/'
-output_path ='D:/2000Wnew/'
+output_path ='D:/2000Wnew/sheng'
 
 area={"11":"北京","12":"天津","13":"河北","14":"山西","15":"内蒙古",
       "21":"辽宁","22":"吉林","23":"黑龙江","31":"上海","32":"江苏",
@@ -75,7 +75,7 @@ def pick_data(filename):
               elif int(aa[2]) == 32:
                   list32.append(aa)
               elif int(aa[2]) == 33:
-                  list31.append(aa)
+                  list33.append(aa)
               elif int(aa[2]) == 34:
                   list34.append(aa)
               elif int(aa[2]) == 35:
@@ -132,7 +132,7 @@ def write_list(list_name):
                 # print(values)
                 write_csv = ''.join(output_path + values + ".csv")
                 # print(write_csv)
-                with open(write_csv,'w',encoding='gb18030',newline='') as csv_write:
+                with open(write_csv,'a',encoding='gb18030',newline='') as csv_write:
                     filewrite = csv.writer(csv_write)
                     for a in list_name:
                         filewrite.writerow(a)
@@ -141,17 +141,19 @@ def main(list_name):
     write_list(list_name)
 
 if __name__ == '__main__':
-    for file in glob.glob(os.path.join(input_filename,'new_*.csv')):
+    # for file in glob.glob(os.path.join(input_filename,'new_*.csv')):
+    #MemoryError,数据太大，机器太差
+    for file in glob.glob(os.path.join(input_filename,'last50000.csv')):
         t_start = time.process_time()
         pick_data(file)
-        t_end = time.process_time()
-        t1 = t_end - t_start
-        print('{0:s}  耗时: {1:.2f} s'.format(file,t1))
         pool = Pool(processes=2)
         pool.map(main,[i for i in [list11,list12,list13,list14,list15,list21,list22,list23,list31,list32,
                                list33,list34,list35,list36,list37,list41,list42,list43,list44,list45,
                                list46,list50,list51,list52,list53,list54,list61,list62,list63,list64,
                                list65,list71,list81,list82,list91]])
     # pool.map(main,[i for i in [list11,list12]])
+        t_end = time.process_time()
+        t1 = t_end - t_start
+        print('{0:s}  耗时: {1:.2f} s'.format(file,t1))
 
 
